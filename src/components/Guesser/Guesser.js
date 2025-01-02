1e7a6a962e6ecd1f069aaa007474296e1a827933
Nick Hoffman <nick@deadorange.com>
2025-01-01 21:49:02 -0500

    Create the Guesser component.

---
+++ src/components/Guesser/Guesser.js
@@ -0,0 +1,47 @@
+import React from "react"
+
+function Guesser() {
+  const [guess, setGuess] = React.useState("")
+  const validGuessPattern = /^[A-Z]+$/
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    if (guess.length !== 5) {
+      console.log("handleSubmit() Length != 5. Aborting.")
+      return
+    }
+
+    console.log({ guess: guess })
+    setGuess("")
+  }
+
+  function handleTyping(event) {
+    const newGuess = event.target.value.toUpperCase()
+
+    if (!validGuessPattern.test(newGuess)) {
+      console.log("handleTyping() Invalid character. Aborting.")
+      return
+    }
+
+    if (newGuess.length > 5) {
+      console.log("handleTyping() Too long. Aborting.")
+      return
+    }
+
+    setGuess(newGuess)
+  }
+
+  return (
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
+      <label htmlFor="guess-input">Enter guess:</label>
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleTyping}
+      />
+    </form>
+  )
+}
+
+export default Guesser
